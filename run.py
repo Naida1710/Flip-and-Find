7c9665682560da6274e0103c2d3e6904a24bf7b7
Naida Hadzic <naida.popovic1993@gmail.com>
2025-05-04 15:02:12 +0200

    Add difficulty levels to game

--- run.py
+++ run.py
@@ -1,31 +1,32 @@
 import tkinter as tk
-from tkinter import ttk
+
+
+class FlipAndFind:
+    def __init__(self, master=None):
+        self.master = master
+        self.master.title("Flip and Find")  # Window title
+        self.master.geometry("900x600")     # Window dimensions
+        self.master.configure(bg="#3a3a3a")  # Window background color
+
+        # Define difficulty levels with symbols
+        self.difficulty_levels = {
+            "Easy": {
+                "grid": (4, 4),
+                "symbols": ["⭐", "❤️", "🔺", "🔺", "🔺", "🔺", "🔺", "🔺"]
+            },
+            "Medium": {
+                "grid": (6, 6),
+                "symbols": ["⭐", "❤️", "🔺", "🔺", "🔺", "🔺", "🔺", "🔺"]
+            },
+            "Hard": {
+                "grid": (8, 8),
+                "symbols": ["⭐", "❤️", "🔺", "🔺", "🔺", "🔺", "🔺", "🔺"]
+            }
+        }
 
 
 # Main application window
 root = tk.Tk()
-root.title("Tkinter in Visual Studio")
-ttk.Label(root, text="Hello, Tkinter!").pack()
-
-
-# Custom window class
-class FlipAndFind:
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.title("Flip and Find")  # Window title
-        self.geometry("900x600")     # Window dimensions
-        self.configure("#3a3a3a")    # Window background color
-        self.custom_font = ("Helvetica, 14, bold")  # Font family
-        self.colors = {
-            'bg': "#3a3a3a",          # Main background color
-            'card_bg': "#0f3469",     # Card background color
-            'card_fg': "#e94560",     # Card foregorund color
-            'sidebar_bg': "#16213e",  # Sidebar background color
-        }
-
-
-# Launch custom window after the first
-flip_window = FlipAndFind(root)
-
+flip_window = FlipAndFind(master=root)
 
 root.mainloop()
