7f321bce8e7afe4f140d582c0b440fd88880d0b2
Naida Hadzic <naida.popovic1993@gmail.com>
2025-05-04 13:31:49 +0200

    Add Tkinter GUI with Flip and Find window

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import tkinter as tk
+from tkinter import ttk
+
+
+# Main application window
+root = tk.Tk()
+root.title("Tkinter in Visual Studio")
+ttk.Label(root, text="Hello, Tkinter!").pack()
+
+
+# Custom window class
+class FlipAndFind:
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.title("Flip and Find")
+        self.geometry("900x600")
+        self.configure("#3a3a3a")
+
+
+# Launch custom window after the first
+flip_window = FlipAndFind(root)
+
+
+root.mainloop()
